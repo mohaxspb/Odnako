4f07bd4f948c4a9d82d1dad8801c7b8449763070
mohaxspb <mohax.spb@gmail.com>
2014-10-19 19:52:13 +0400

    add ActivityMain

---
+++ src/ru/kuchanov/odnako/activities/ActivityMain.java
@@ -0,0 +1,23 @@
+/*
+ 19.10.2014
+ActivityMain.java
+Created by Kuchanov Yuri,
+mohax.spb@gmail.com
+ */
+package ru.kuchanov.odnako.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class ActivityMain extends ActionBarActivity
+{
+
+	
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		System.out.println("ActivityMain onCreate");
+		super.onCreate(savedInstanceState);
+		
+		
+	}
+}
