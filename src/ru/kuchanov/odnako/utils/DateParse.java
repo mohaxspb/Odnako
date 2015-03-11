12fb5f7b49960c1d21c811ea1741df65b1c2c06c
mohaxspb <mohax.spb@gmail.com>
2015-03-12 01:44:51 +0400

    Fix wrong parsing of date if year is not current just by replacing DateFormats examples in DateParsing utility;

--- src/ru/kuchanov/odnako/utils/DateParse.java
+++ src/ru/kuchanov/odnako/utils/DateParse.java
@@ -14,18 +14,12 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 
-import android.util.Log;
-
 public class DateParse
 {
+	final static String LOG = DateParse.class.getSimpleName() + "/";
 	//test values, that we can recive from site
 	static final String dateStrEng = "Fri, 12 Dec 2014 22:06:00 +0400";
 	static final String dateStrRu = "2 июня 2009";
-
-	public DateParse()
-	{
-		Log.d("", "DateParse constructor called");
-	}
 
 	public static Date parse(String stringDate)
 	{
@@ -33,14 +27,14 @@ public class DateParse
 		DateFormat fEng = new SimpleDateFormat("E, d MMM yyyy HH:mm:ss Z", new Locale("en"));
 		DateFormat today = new SimpleDateFormat("HH:mm", new Locale("ru"));
 		DateFormat thisYear = new SimpleDateFormat("dd/MM", new Locale("ru"));
-		DateFormat prevYear = new SimpleDateFormat("dd/MM/yyyy", new Locale("ru"));
+		DateFormat prevYear = new SimpleDateFormat("dd/MM/yy", new Locale("ru"));
 
 		DateFormat[] dateFormatArr = new DateFormat[5];
 		dateFormatArr[0] = fRu;
 		dateFormatArr[1] = fEng;
-		dateFormatArr[2] = today;
+		dateFormatArr[2] = prevYear;
 		dateFormatArr[3] = thisYear;
-		dateFormatArr[4] = prevYear;
+		dateFormatArr[4] = today;
 		Date d = null;
 		for (int i = 0; i < dateFormatArr.length && d == null; i++)
 		{
@@ -48,7 +42,6 @@ public class DateParse
 			{
 				d = dateFormatArr[i].parse(stringDate);
 				//here set year, month, day
-				//TODO
 				//create calendar with curent Y, M, D and MOSCOW TZ
 				Calendar calNow = Calendar.getInstance(TimeZone.getTimeZone("GMT+03:00"), new Locale("ru"));
 				calNow.set(calNow.get(Calendar.YEAR), calNow.get(Calendar.MONTH),
@@ -64,7 +57,7 @@ public class DateParse
 					case 1:
 					//set only locale
 					break;
-					case 2:
+					case 4:
 						//set Year, month and day and TimeZone
 						//set to calendar with given date Y, M, D and TZ from calendar above
 						calGiven.set(calNow.get(Calendar.YEAR), calNow.get(Calendar.MONTH),
@@ -81,12 +74,13 @@ public class DateParse
 						//finally set new date
 						d.setTime(calGiven.getTimeInMillis());
 					break;
-					case 4:
+					case 2:
 						//set timeZone
 						//set to calendar with given date TZ from calendar above
 						calGiven.setTimeZone(calNow.getTimeZone());
 						//finally set new date
 						d.setTime(calGiven.getTimeInMillis());
+//						Log.e(LOG, "case 2 in DateParse, dd/MM/yy: " + calGiven.toString());
 					break;
 				}
 			} catch (ParseException e)
