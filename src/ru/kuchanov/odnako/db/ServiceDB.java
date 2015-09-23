963cda13321cffc47d66cd9426bd01c8aa1f4e31
mohaxspb <mohax.spb@gmail.com>
2015-09-23 21:04:49 +0400

    Change version number to 330 Just ready for release...

--- src/ru/kuchanov/odnako/db/ServiceDB.java
+++ src/ru/kuchanov/odnako/db/ServiceDB.java
@@ -8,7 +8,6 @@ package ru.kuchanov.odnako.db;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
@@ -581,8 +580,8 @@ CallbackWriteFromBottom, CallbackWriteFromTop, /* CallbackWriteArticles, */Callb
 	public void sendBroadcastWithResult(Context ctx, String[] resultMessage, ArrayList<Article> dataToSend,
 	String categoryToLoad, int pageToLoad)
 	{
-		Log.d(LOG + categoryToLoad, "sendBroadcastWithResult");
-		Log.e(LOG, Arrays.toString(resultMessage));
+//		Log.d(LOG + categoryToLoad, "sendBroadcastWithResult");
+//		Log.e(LOG, Arrays.toString(resultMessage));
 		if(!Msg.ERROR.equals(resultMessage[0])&&!Const.Error.CONNECTION_ERROR.equals(resultMessage[1]))
 		{
 			updateHashMap(dataToSend, categoryToLoad, pageToLoad);
